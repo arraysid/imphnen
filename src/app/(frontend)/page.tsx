cb3ad104ce5189a9a9609f2cf165f69010b14fd9
Muharu <arraysdotid@gmail.com>
2025-04-10 19:32:31 +0700

    feat: integrate ui library

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default async function HomePage() {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Click Me</Button>
     </main>
-  )
+  );
 }
