40e7b159d45c40586abbeaa3a681f9d6a36ec34a
Muharu <arraysdotid@gmail.com>
2025-04-10 20:15:14 +0700

    feat: add navbar and card components, update hero section styling, and include react-icons

--- src/app/(frontend)/(landing)/_components/hero-section.tsx
+++ src/app/(frontend)/(landing)/_components/hero-section.tsx
@@ -1,3 +1,5 @@
 export function HeroSection() {
-  return <section className="bg-main h-[100dvh] border-b-8"></section>;
+  return (
+    <section className="h-[calc(100dvh-5rem)] border-b-8 bg-[#C7EAFB]"></section>
+  );
 }
---
+++ src/app/(frontend)/_components/navbar.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { FaDiscord, FaFacebookF } from "react-icons/fa6";
+
+export function Navbar() {
+  return (
+    <header className="h-20 border-b-8">
+      <nav className="mx-auto flex h-full max-w-5xl items-center justify-between">
+        <div className="flex gap-x-6">
+          <Link href="/" className="text-xl font-bold">
+            Home
+          </Link>
+          <Link href="/" className="text-xl font-bold">
+            Event
+          </Link>
+          <Link href="/" className="text-xl font-bold">
+            Store
+          </Link>
+        </div>
+        <div className="flex gap-x-3">
+          <Button size="icon" className="bg-white">
+            <FaFacebookF className="size-5" />
+          </Button>
+          <Button size="icon" className="bg-white">
+            <FaDiscord className="size-5" />
+          </Button>
+        </div>
+      </nav>
+    </header>
+  );
+}
