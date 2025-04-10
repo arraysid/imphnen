bbff3c1b01c26e85b51b817fd75ec51d63b44ff0
Muharu <arraysdotid@gmail.com>
2025-04-10 18:52:31 +0700

    feat: cleanup project

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -2,18 +2,13 @@ import React from 'react'
 import './styles.css'
 
 export const metadata = {
-  description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
+  title: 'IMPHNEN',
 }
 
-export default async function RootLayout(props: { children: React.ReactNode }) {
-  const { children } = props
-
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>
-        <main>{children}</main>
-      </body>
+    <html lang="en" suppressHydrationWarning>
+      <body>{children}</body>
     </html>
   )
 }
