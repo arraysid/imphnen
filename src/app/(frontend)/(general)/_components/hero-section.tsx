f1bad15971117e170e241c11b92d3b3e30a48783
Muharu <arraysdotid@gmail.com>
2025-04-11 11:22:15 +0700

    feat: remove unused images and enhance hero and stats sections layout; add star component and create profile button

--- src/app/(frontend)/(general)/_components/hero-section.tsx
+++ src/app/(frontend)/(general)/_components/hero-section.tsx
@@ -29,6 +29,21 @@ export function HeroSection() {
       className="relative flex h-[calc(100dvh-5rem)] items-center justify-center px-4 md:h-[calc(100dvh-6rem)]"
     >
       <div className="absolute inset-0 -z-10 h-full w-full bg-[#C7EAFB] bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"></div>
+
+      <Star className="absolute top-5 left-5 lg:top-10 lg:left-10" size={50} />
+      <Star
+        className="absolute top-5 right-5 lg:top-10 lg:right-10"
+        size={50}
+      />
+      <Star
+        className="absolute right-5 bottom-5 lg:right-10 lg:bottom-10"
+        size={50}
+      />
+      <Star
+        className="absolute bottom-5 left-5 lg:bottom-10 lg:left-10"
+        size={50}
+      />
+
       <motion.div
         variants={containerVariants}
         initial="hidden"
@@ -56,8 +71,14 @@ export function HeroSection() {
 
         <motion.div
           variants={childVariants}
-          className="flex flex-col items-center gap-4 sm:justify-center"
+          className="flex flex-col items-center gap-4 sm:justify-center lg:flex-row"
         >
+          <Button
+            size="lg"
+            className="flex items-center gap-x-2 bg-white hover:bg-gray-50"
+          >
+            <span>Create Profile</span>
+          </Button>
           <Button
             size="lg"
             className="flex items-center gap-x-2 bg-white hover:bg-gray-50"
@@ -68,5 +89,41 @@ export function HeroSection() {
         </motion.div>
       </motion.div>
     </motion.section>
+  );
+}
+
+function Star({
+  color,
+  size,
+  stroke,
+  strokeWidth,
+  pathClassName,
+  width,
+  height,
+  ...props
+}: React.SVGProps<SVGSVGElement> & {
+  color?: string;
+  size?: number;
+  stroke?: string;
+  pathClassName?: string;
+  strokeWidth?: number;
+}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 200 200"
+      width={size ?? width}
+      height={size ?? height}
+      {...props}
+    >
+      <path
+        fill={color ?? "currentColor"}
+        stroke={stroke}
+        strokeWidth={strokeWidth}
+        className={pathClassName}
+        d="M100 5s5.088 49.035 25.527 69.473C145.965 94.912 195 100 195 100s-49.035 5.088-69.473 25.527C105.088 145.965 100 195 100 195s-5.088-49.035-25.527-69.473C54.035 105.088 5 100 5 100s49.035-5.088 69.473-25.527C94.912 54.035 100 5 100 5"
+      />
+    </svg>
   );
 }
--- src/app/(frontend)/(general)/_components/stats-section.tsx
+++ src/app/(frontend)/(general)/_components/stats-section.tsx
@@ -2,12 +2,7 @@ export function StatsSection() {
   return (
     <section className="relative h-[40dvh] border-b-4 py-4">
       <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"></div>
-      <div className="mx-auto hidden h-full max-w-5xl flex-row items-center justify-center gap-x-20 gap-y-4 lg:flex">
-        <Stats stats="160K+" description="Members on Facebook" />
-        <Stats stats="5K+" description="Members on Discord" />
-        <Stats stats="1K+" description="Quality Meme Post" />
-      </div>
-      <div className="flex h-full flex-col items-center justify-center gap-y-6 lg:hidden">
+      <div className="mx-auto flex h-full max-w-5xl flex-col items-center justify-center gap-6 lg:flex-row lg:gap-20">
         <Stats stats="160K+" description="Members on Facebook" />
         <Stats stats="5K+" description="Members on Discord" />
         <Stats stats="1K+" description="Quality Meme Post" />
