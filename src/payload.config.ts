72ee754ab7eb9ce100de8e0bf73a3f44409f3a4d
Muharu <arraysdotid@gmail.com>
2025-04-25 14:19:06 +0700

    feat: integrate S3 storage adapter and update payload configuration

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,7 +1,9 @@
-// storage-adapter-import-placeholder
 import { postgresAdapter } from "@payloadcms/db-postgres";
 import { payloadCloudPlugin } from "@payloadcms/payload-cloud";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import { s3Storage } from "@payloadcms/storage-s3";
+import { NodeHttpHandler } from "@smithy/node-http-handler";
+import https from "https";
 import path from "path";
 import { buildConfig } from "payload";
 import sharp from "sharp";
@@ -35,6 +37,23 @@ export default buildConfig({
   sharp,
   plugins: [
     payloadCloudPlugin(),
-    // storage-adapter-placeholder
+    s3Storage({
+      bucket: "cms",
+      config: {
+        endpoint: process.env.STORAGE_ENDPOINT,
+        credentials: {
+          accessKeyId: process.env.STORAGE_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.STORAGE_SECRET_ACCESS_KEY!,
+        },
+        region: process.env.STORAGE_REGION!,
+        forcePathStyle: true,
+        requestHandler: new NodeHttpHandler({
+          httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+        }),
+      },
+      collections: {
+        media: true,
+      },
+    }),
   ],
 });
